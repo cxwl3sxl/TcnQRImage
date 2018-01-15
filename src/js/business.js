67f7bd2ed2ca5317218ab8e4efa48524e5cb8530
cxwl3sxl <xinglin.sc@outlook.com>
2018-01-15 16:50:05 +0800

    代码优化

--- src/js/business.js
+++ src/js/business.js
@@ -1,23 +1,41 @@
 $(function () {
     chrome.tabs.getSelected(function (tab) {
-        $.get("https://api.weibo.com/2/short_url/shorten.json", {
-            source: "5786724301",
-            url_long: tab.url
-        }, function (r) {
-            if (r.urls.length == 0) {
-                showMessage("服务器无返回");
+        try {
+            var url = tab.url;
+            if (url == "" || url.indexOf("chrome:") == 0) {
+                showMessage("地址错误");
+                console.warn("地址错误");
                 return;
             }
-            if (r.urls[0].type != 0) {
-                showMessage("错误:" + r.urls[0].type);
-                return;
-            }
-            var url = r.urls[0].url_short;
-            showMessage(url);
-            $("#qrCodeImage").attr("src", "http://qr.liantu.com/api.php?text=" + url);
-        }).fail(function () {
-            showMessage("请求失败，请重试！");
-        });
+            $.get("https://api.weibo.com/2/short_url/shorten.json", {
+                source: "5786724301",
+                url_long: url
+            }, function (r) {
+                try {
+                    if (r.urls.length == 0) {
+                        showMessage("服务器无返回");
+                        console.warn("服务器无返回");
+                        return;
+                    }
+                    if (r.urls[0].type != 0) {
+                        showMessage("错误:" + r.urls[0].type);
+                        console.warn("错误:" + r.urls[0].type);
+                        return;
+                    }
+                    var url = r.urls[0].url_short;
+                    showMessage(url);
+                    $("#qrCodeImage").attr("src", "http://qr.liantu.com/api.php?text=" + url);
+                } catch (e) {
+                    showMessage(e);
+                }
+            }).fail(function () {
+                showMessage("请求失败，请重试！");
+                console.warn("请求失败，请重试！");
+            });
+        } catch (e) {
+            showMessage(e);
+            console.warn(e);
+        }
     });
 });
 
