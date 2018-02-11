087a326bf5721bd99013026fdc42935d3bb63d5e
cxwl3sxl <xinglin.sc@outlook.com>
2018-02-11 13:55:22 +0800

    将新浪接口改为百度接口

--- src/js/business.js
+++ src/js/business.js
@@ -7,24 +7,18 @@ $(function () {
                 console.warn("地址错误");
                 return;
             }
-            $.get("https://api.weibo.com/2/short_url/shorten.json", {
-                source: "5786724301",
-                url_long: url
+            $.post("http://dwz.cn/create.php", {
+                url: url
             }, function (r) {
                 try {
-                    if (r.urls.length == 0) {
+                    if (r == null) {
                         showMessage("服务器无返回");
                         console.warn("服务器无返回");
                         return;
                     }
-                    if (r.urls[0].type != 0) {
-                        showMessage("错误:" + r.urls[0].type);
-                        console.warn("错误:" + r.urls[0].type);
-                        return;
-                    }
-                    var url = r.urls[0].url_short;
-                    showMessage(url);
-                    $("#qrCodeImage").attr("src", "http://qr.liantu.com/api.php?text=" + url);
+                    var jr = JSON.parse(r);
+                    showMessage(jr.tinyurl);
+                    $("#qrCodeImage").attr("src", "http://qr.liantu.com/api.php?text=" + jr.tinyurl);
                 } catch (e) {
                     showMessage(e);
                 }
